a817b48db0f9463bbddfd550f0ff3ec16cbc9103
guieusebiogaspar <guicr9@hotmail.com>
2021-03-23 15:15:23 +0000

    mais metodos no rmi. criação da admin console

--- src/RMI/RMIServerInterface.java
+++ src/RMI/RMIServerInterface.java
@@ -3,14 +3,25 @@ package RMI;
 import java.rmi.*;
 
 public interface RMIServerInterface extends Remote {
+
+    public void olaAdmin(AdminConsoleInterface adm) throws RemoteException;
+
+    public void showMenu() throws RemoteException;
+
+    public void readCommand(String command) throws RemoteException;
+
     // isto tudo depois é substituiido por um objeto client
     public void register(String name, String nickname, String password, String phone, String morada, String cc, String ncc) throws java.rmi.RemoteException;
     // Aqui será tambem objeto (?) e falta pensar na maneira de restringir as eleições
-    public void create_election(String startDate, String endDate, String title, String description) throws RemoteException;
+    public void createElection(String startDate, String endDate, String title, String description) throws RemoteException;
     // objeto
-    public void edit_election(String startDate, String endDate, String title, String description) throws RemoteException;
+    public void editElection(String startDate, String endDate, String title, String description) throws RemoteException;
     // objeto
-    public void end_election(String startDate, String endDate, String title, String description) throws RemoteException;
+    public void endElection(String startDate, String endDate, String title, String description) throws RemoteException;
+
+    public void writeBD() throws RemoteException;
+
+    public void readBD() throws RemoteException;
 
     public void check_results() throws RemoteException;
 }
